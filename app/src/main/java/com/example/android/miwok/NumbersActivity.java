f927da07d67b6f1972c16962e38a0191c3e1a1f0
Gideon <gideonchiong@gmail.com>
2016-10-02 12:00:34 +0800

    Added While loop

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,7 +2,8 @@ package com.example.android.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -26,8 +27,18 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        Log.v("NumberACtivity","Word at index 0: "+ words.get(0));
-        Log.v("NumberACtivity","Word at index 1: "+ words.get(1));
+        int count = 0;
+
+        while (count < 10) {
+            LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(count));
+            rootView.addView(wordView);
+
+            count++;
+
+        }
+
 
     }
 }
